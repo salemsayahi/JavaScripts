07f72a08ac83bcfc61c53e56a1cec28f7ed42c26
salemsayahi <salem.sayahi@gmail.com>
2020-10-06 23:28:01 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -56,11 +56,14 @@ function Ajout4() {
     var sti=parseFloat(ST.textContent);
     ST.textContent= sti+pu;
 }
-function Supprimer1() {
+function Supprimer1(id) {
+ 
   var elem = document.getElementById("qt1");
+ 
   var qt = parseInt(elem.textContent);
-  qt--;
+  
   if (qt > 0) {
+    qt--;
     elem.textContent = qt;
     var pu = parseFloat(document.getElementById("pu1").textContent);
     var Total1 = document.getElementById("t1");
@@ -69,15 +72,14 @@ function Supprimer1() {
     var ST=document.getElementById("ST");
     var sti=parseFloat(ST.textContent);
     ST.textContent= sti-pu;
-  } else {
-    document.getElementById("panier").deleteRow(1);
-  }
+  } 
 }
 function Supprimer2() {
     var elem = document.getElementById("qt2");
     var qt = parseInt(elem.textContent);
-    qt--;
+ 
     if (qt > 0) {
+      qt--;
       elem.textContent = qt;
       var pu = parseFloat(document.getElementById("pu2").textContent);
       var Total1 = document.getElementById("t2");
@@ -86,15 +88,14 @@ function Supprimer2() {
       var ST=document.getElementById("ST");
     var sti=parseFloat(ST.textContent);
     ST.textContent= sti-pu;
-    } else {
-      document.getElementById("panier").deleteRow(2);
-    }
+    } 
   }
   function Supprimer3() {
     var elem = document.getElementById("qt3");
     var qt = parseInt(elem.textContent);
-    qt--;
+
     if (qt > 0) {
+      qt--;
       elem.textContent = qt;
       var pu = parseFloat(document.getElementById("pu3").textContent);
       var Total1 = document.getElementById("t3");
@@ -103,15 +104,14 @@ function Supprimer2() {
       var ST=document.getElementById("ST");
     var sti=parseFloat(ST.textContent);
     ST.textContent= sti-pu;
-    } else {
-      document.getElementById("panier").deleteRow(3);
-    }
+    } 
   }
   function Supprimer4() {
     var elem = document.getElementById("qt4");
     var qt = parseInt(elem.textContent);
-    qt--;
+
     if (qt > 0) {
+      qt--;
       elem.textContent = qt;
       var pu = parseFloat(document.getElementById("pu4").textContent);
       var Total1 = document.getElementById("t4");
@@ -120,7 +120,93 @@ function Supprimer2() {
       var ST=document.getElementById("ST");
     var sti=parseFloat(ST.textContent);
     ST.textContent= sti-pu;
-    } else {
-      document.getElementById("panier").deleteRow(4);
-    }
+    } 
   }
+  function Supprimer(id) {
+    document.getElementById("panier").deleteRow(parseInt(id));
+  }
+
+  document.getElementById("produits").style.display = "subproduit";
+
+  function commande()
+  {
+    document.getElementById("produits").style.display = "none";
+    
+    document.getElementById("cmd").style.display = "none";
+    document.getElementById("panier").style.display="table" ;
+  }
+  function like(id)
+  { var el=document.getElementById("like"+id);
+    var nb = parseFloat(el.textContent)+1;
+    el.textContent=nb;
+
+  }
+  function dislike(id)
+  { var el=document.getElementById("dislike"+id);
+    var nb = parseFloat(el.textContent)+1;
+    el.textContent=nb;
+
+  }
+  function AjoutProduit()
+  { var el=document.getElementById("nbproduit");
+    var nb = parseFloat(el.textContent)+1;
+    el.textContent=nb;
+
+  }
+  
+  /*
+  function AjoutProduit()
+   {
+      var body = document.getElementsByTagName("body")[0];
+
+    // create elements <table> and a <tbody>
+    
+    var tblBody = document.getElementById("panier");
+  
+    // cells creation
+ 
+      var row = document.createElement("tr");
+  
+  
+        var nom = document.createElement("td");
+             var cellText =  document.createTextNode($(this).childNodes+"");;
+  
+        nom.appendChild(cellText);
+        row.appendChild(nom);
+        var nom = document.createElement("td");
+        var cellText = document.createTextNode("rrrrrrrrrrrtest");
+  
+        nom.appendChild(cellText);
+        row.appendChild(nom);
+        var nom = document.createElement("td");
+        var cellText = document.createTextNode("rrrrrrrrrrrtest");
+  
+        nom.appendChild(cellText);
+        row.appendChild(nom);
+        var nom = document.createElement("td");
+        var cellText = document.createTextNode("rrrrrrrrrrrtest");
+  
+        nom.appendChild(cellText);
+        row.appendChild(nom);
+        var nom = document.createElement("td");
+        var cellText = document.createTextNode("rrrrrrrrrrrtest");
+  
+        nom.appendChild(cellText);
+        row.appendChild(nom);
+      
+  
+      //row added to end of table body
+      tblBody.appendChild(row);
+    
+  
+    // append the <tbody> inside the <table>
+    tbl.appendChild(tblBody);
+    // put <table> in the <body>
+    body.appendChild(tbl);
+    // tbl border attribute to 
+   // tbl.setAttribute("border", "2");
+  
+
+  }*/
+
+ 
